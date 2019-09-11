74054087a49b5e4945a492275e56ae4f535e58c6
caspianzx <caspianzx@gmail.com>
2019-09-11 11:27:32 +0800

    created links for contact page

--- src/client/testing.jsx
+++ src/client/testing.jsx
@@ -14,6 +14,11 @@ function NavBar() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
+                    <Link to="/">
+                        <a className="nav-link" href="#" id="navLink">Home</a>
+                    </Link>
+                    </li>
+                    <li className="nav-item">
                         <Link to="/service">
                         <a className="nav-link" href="#" id="navLink">Service
                         </a>
@@ -49,7 +54,7 @@ function Home() {
             <div className ="row">
                 <div className ="col-10 offset-1">
                     <div className= {styles.aboutus} >ABOUT US</div>
-                    <p className={styles.intro}><span className ={styles.brand}>PARCO</span> is an application that tracks car park lot availability around Singapore real-time. Features include a search function that is able to filter for car parks that are near the  search query, the ability to alert the users if there are little lots in the carpark, and lastly, it informs users whether free parking is available.<br/>Interested? Click the link below to get started!</p>
+                    <p className={styles.intro}><span className ={styles.brand}>PARCO</span> is an application that tracks car park lots availability around Singapore using real-time data. It features a search function that is able to filter for car parks that are near the  search query, the ability to alert the users if there are little lots in the carpark, and informs users whether free parking is available.<br/>Interested? Click the link below to get started!</p>
                 </div>
             </div>
             <div className ="row">
@@ -78,8 +83,8 @@ function Contact() {
                     <div className ={styles.foundername}>Caspian Ng</div>
                     <div className ={styles.founder}>founder</div>
                     <div className ={styles.links}>
-                        <span className="fab fa-github" id={styles.linkspace}></span>
-                        <span className="fab fa-linkedin-in" id={styles.linkspace}></span>
+                        <a href="https://github.com/caspianzx"><span className="fab fa-github" id={styles.linkspace}></span></a>
+                        <a href="https://www.linkedin.com/in/caspian-ng/"><span className="fab fa-linkedin-in" id={styles.linkspace}></span></a>
                         <span className="fab fa-facebook-f" id={styles.linkspace}></span>
                     </div>
                 </div>
